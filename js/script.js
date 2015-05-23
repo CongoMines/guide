ebb32425e2fd427d1272360e293a03efafab5e2c
James Conkling <james.lane.conkling@gmail.com>
2015-05-23 18:40:41 +0100

    init repo

---
+++ js/script.js
@@ -0,0 +1,14 @@
+(function(){
+  var guide = {
+    init: function(){
+      $(document).foundation();
+      console.log('init');
+    }
+  };
+
+  guide.init();
+
+  window.guide = guide;
+})();
+
+      
